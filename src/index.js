5fa2830ad0d0e587f0ba5b8da2a725a133d1f6f2
biljana_kotev <biljanakotev@gmail.com>
2023-03-04 18:10:20 +0100

    added html/css/js files

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const startBtn = document.getElementById("start-btn");
+const svgImg = document.getElementById("svg-img");
+const audio = new Audio("https://vgmsite.com/soundtracks/donkey-kong-arcade/rtchzzqw/03%20Stage%20Opening%201.mp3");
+
+startBtn.addEventListener("click", function () {
+  startBtn.style.display = "none";
+  svgImg.style.display = "block";
+  audio.play();
+});
+
+function playAudio() {
+  audio.play();
+}
